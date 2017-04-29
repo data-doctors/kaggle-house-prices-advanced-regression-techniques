72e4af35cf64d92a89debd474a2c7030846c2fee
Robert Jonczy <robert.jonczy@gmail.com>
2017-04-29 02:06:23 +0200

    Allows to bulk save all models from bash script

--- run_all.sh
+++ run_all.sh
@@ -3,17 +3,17 @@
 # base/single models
 
 # trees algorithms
-python models/single/model_dt.py
-python models/single/model_rf.py
-python models/single/model_et.py
-python models/single/model_xgb.py
-python models/single/model_lgb.py
+python models/single/model_dt.py $1
+python models/single/model_rf.py $1
+python models/single/model_et.py $1
+python models/single/model_xgb.py $1
+python models/single/model_lgb.py $1
 
 
 # linear algorithms
-python models/single/model_lasso.py
-python models/single/model_ridge.py
-python models/single/model_elasticnet.py
+python models/single/model_lasso.py $1
+python models/single/model_ridge.py $1
+python models/single/model_elasticnet.py $1
 
 # stacking
 # python models/ensembles/model_stack_l1.py
