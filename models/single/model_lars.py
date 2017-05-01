104eac9e7bfde779c8163b71e5e071bf0a56de93
Robert Jonczy <robert.jonczy@gmail.com>
2017-05-01 02:13:31 +0200

    Added Lars model

---
+++ models/single/model_lars.py
@@ -0,0 +1,66 @@
+import pandas as pd 
+import numpy as np
+from matplotlib import pyplot
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_squared_error
+
+import sys
+import time
+import warnings
+
+from sklearn import linear_model
+
+warnings.filterwarnings('ignore')
+
+folds = 5
+seed = 7
+
+model_label='lars'
+
+def rmse_cv(model, X_train, y_train):
+    kfold = KFold(n_splits=folds, random_state=seed)
+    rmse = np.sqrt(-cross_val_score(model, X_train, y_train, scoring="neg_mean_squared_error", cv = kfold))
+    return(rmse)
+
+def main(predictions = False):
+
+    train = pd.read_csv("./data/X_train_v1.csv")
+
+    y_train = train['SalePrice']
+    X_train = train.loc[:,'MSSubClass':'SaleCondition_Partial']
+
+    test = pd.read_csv("./data/X_test_v1.csv")
+    X_test = test.loc[:,'MSSubClass':'SaleCondition_Partial']
+
+    # build model
+    model = linear_model.Lars(n_nonzero_coefs=61)
+
+    # fit model
+    model.fit(X_train, y_train)
+
+    # evaluate model
+    results = rmse_cv(model, X_train, y_train)
+    print("RMSE-{}-CV({})={:06.5f}+-{:06.5f}".format(model_label, folds, results.mean(), results.std()))
+
+    # # predict
+    if predictions:
+        y_test_pred_log = model.predict(X_test)
+        y_test_pred = np.expm1(y_test_pred_log)
+        submission = pd.DataFrame({'Id':test['Id'], 'SalePrice':y_test_pred})
+
+        subFileName = "./submissions/sub-" + model_label + "-" + time.strftime("%Y%m%d-%H%M%S") + ".csv"
+        print("saving to file: " + subFileName)
+        submission.to_csv(subFileName, index=False)
+
+# ----------------------------------------------------------------------------
+
+
+if __name__ == "__main__":
+    savePredict = False
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'save':
+            savePredict = True
+    main(predictions = savePredict)
---
+++ models/tuning/model_lars_gridtune.py
@@ -0,0 +1,73 @@
+import pandas as pd 
+import numpy as np
+from matplotlib import pyplot
+
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import make_scorer
+
+import sys
+import time
+
+from sklearn import linear_model
+
+folds = 5
+seed = 7
+
+model='lars'
+
+def rmse(y_true, y_pred):
+    return np.sqrt(mean_squared_error(y_true, y_pred))
+
+def load_data(version = 'v1'):
+
+    train = pd.read_csv("./data/X_train_" + version + ".csv")
+
+    y_train = train['SalePrice']
+    X_train = train.loc[:,'MSSubClass':'SaleCondition_Partial']
+
+    test = pd.read_csv("./data/X_test_" + version + ".csv")
+    X_test = test.loc[:,'MSSubClass':'SaleCondition_Partial']
+
+    return X_train, y_train, X_test
+
+def main():
+    
+    X_train, y_train, X_test = load_data('v1')
+
+    # tunning
+    model = linear_model.Lars()
+
+    # dict with tunning parameters
+    param_grid = {
+        'n_nonzero_coefs': range(50, 70, 1)
+    }
+
+    kfold = KFold(n_splits=folds, random_state=seed)
+
+    scorer = make_scorer(rmse, greater_is_better=False)
+    grid_search = GridSearchCV(model, param_grid, n_jobs=-1, cv=kfold, verbose=1, scoring=scorer)
+    grid_result = grid_search.fit(X_train, y_train)
+
+    means = grid_result.cv_results_['mean_test_score']
+    stds = grid_result.cv_results_['std_test_score']
+    params = grid_result.cv_results_['params']
+
+    for mean, stdev, param in zip(means, stds, params):
+        #print("%f (%f) with: %r" % (mean, stdev, param))
+        print("{:06.5f} ({:06.5f}) with {}".format(mean, stdev, param))
+
+    # summarize results
+    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
